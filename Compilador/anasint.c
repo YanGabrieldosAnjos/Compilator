a61ca333544bcd7bbda0554182951a2ab2c308bc
Sérgio Sacramento <sergiosacramentosouza@gmail.com>
2018-12-10 21:16:51 -0300

    Resolvendo bug de processamento

--- Compilador/anasint.c
+++ Compilador/anasint.c
@@ -1,44 +1,54 @@
 #include "anasint.h"
 
 Token t;
-Token* vetorToken[10000];
-int x=0;
-int estadoToken=0;
+Token *vetorToken[10000];
+int x = 0;
+int estadoToken = 0;
 Pilha *v;
 //Operações de pilha para  o vetor
-Pilha* inicializa_pilha(void){
+Pilha *inicializa_pilha(void)
+{
     return NULL;
 }
 
-Pilha* criapilha(Token *v){
-Pilha* novo=(Pilha*)malloc(sizeof(Pilha));
-  novo->info=v;
-  return novo;
+Pilha *criapilha(Token *v)
+{
+    Pilha *novo = (Pilha *)malloc(sizeof(Pilha));
+    novo->info = v;
+    return novo;
 }
 
-Pilha* push(Pilha *p,Token  *v){
-Pilha *paux=p;
-Pilha *novo=criapilha(v);
+Pilha *push(Pilha *p, Token *v)
+{
+    Pilha *paux = p;
+    Pilha *novo = criapilha(v);
 
-    if(paux==NULL){
-      paux=novo;
-    }else{
-    novo->topo=paux;
+    if (paux == NULL)
+    {
+        paux = novo;
     }
-   return novo;
+    else
+    {
+        novo->topo = paux;
+    }
+    return novo;
 }
-Pilha* pop(Pilha *p){
-Pilha *paux=p;
- Pilha *rem=NULL;
-   if(paux==NULL){
-    printf("nao foi possivel realizar o pop");
-    exit(1);
-   }else{
-   rem=paux;
-   paux=paux->topo;
-   free(rem);
-   }
-   return paux;
+Pilha *pop(Pilha *p)
+{
+    Pilha *paux = p;
+    Pilha *rem = NULL;
+    if (paux == NULL)
+    {
+        printf("nao foi possivel realizar o pop");
+        exit(1);
+    }
+    else
+    {
+        rem = paux;
+        paux = paux->topo;
+        free(rem);
+    }
+    return paux;
 }
 
 //CONVERTENDO TIPOS
@@ -54,81 +64,96 @@ char typeChar(Token *t)
 {
     return t->caracter;
 }
-int isID(Token *t){
-    if(t->tip==ID){
+int isID(Token *t)
+{
+    if (t->tip == ID)
+    {
         return 1;
     }
     return 0;
 }
-int isType(Token  *t){
-    if(t->tip==CHAR||t->tip==INT||t->tip==STRING||t->tip==BOOL||t->tip==REAL){
+int isType(Token *t)
+{
+    if (t->tip == CHAR || t->tip == INT || t->tip == STRING || t->tip == BOOL || t->tip == REAL)
+    {
         return 1;
     }
 }
-int isEscope(Token  *t){
-    if(t->tip==PROG||t->tip==PROC||t->tip==FUNC){
+int isEscope(Token *t)
+{
+    if (t->tip == PROG || t->tip == PROC || t->tip == FUNC)
+    {
         return 1;
     }
 }
-int prog(int  j){
+int prog(int j)
+{
     return j;
 }
 
-int var(int j){
-    v->escopo=0;
-    while(vetorToken[j]->tip!=ENDVAR){
-        if(isType(vetorToken[j])){
-            v=push(v,vetorToken[j]);
+int var(int j)
+{
+    v->escopo = 0;
+    while (vetorToken[j]->tip != ENDVAR)
+    {
+        if (isType(vetorToken[j]))
+        {
+            v = push(v, vetorToken[j]);
             j++;
-            if(isID(vetorToken[j])){
-               v=push(v,vetorToken[j]); 
-               j++;
-            }else{
+            if (isID(vetorToken[j]))
+            {
+                v = push(v, vetorToken[j]);
+                j++;
+            }
+            else
+            {
                 printf("erro: sem identificador");
             }
-            printf("erro:sem tipo");     
+            printf("erro:sem tipo");
         }
-        if(isEscope(vetorToken[j])){
+        if (isEscope(vetorToken[j]))
+        {
             printf("erro:Sem  o endvar");
             break;
-            
-        }    
-          
+        }
     }
 
     return j;
-
 }
-void start(){
-    int z;
-    for(z=0;z<15;z++){
-        printf("AQUI BUCETA%s\n",vetorToken[z]->palavra);
-    }
-    int  j=0;
-    printf("kidisgraça\n");
-    
-    if(vetorToken[j]->tip==14){
-        if(!isID(vetorToken[j])){
-           printf("erro aqui\n"); 
+void start()
+{
+    int j = 0;
+
+    if (vetorToken[j]->tip == 14)
+    {
+        if (!isID(vetorToken[j]))
+        {
+            printf("erro aqui\n");
         }
         j++;
-        if(vetorToken[j]->tip=VAR){
-            j=var(j);
+        if (vetorToken[j]->tip = VAR)
+        {
+            j = var(j);
         }
-        while(vetorToken[j]->tip==FUNC||vetorToken[j]->tip==PROC){
-            j=prog(j);
+        while (vetorToken[j]->tip == FUNC || vetorToken[j]->tip == PROC)
+        {
+            j = prog(j);
         }
-    }else{
+    }
+    else
+    {
         printf("erro SEM pl\n");
     }
-    
 }
-int anasint(Token *t,int i) {
-     vetorToken[i]=t;
-     printf("AQUI DESGRAÇA : %s",vetorToken[i]->palavra);   
-     i++;
-     if(strcmp(vetorToken[i]->palavra,"\n")){
-         printf("AQUI FETO DO SATANAS");
-     }
-     return i;
+int anasint(Token *t, int i)
+{
+
+    printf("%s\n", t->palavra);
+    printf("%d\n", t->tip);
+
+    vetorToken[i] = t;
+
+    start();
+
+    return i;
 }
--- Compilador/main.c
+++ Compilador/main.c
@@ -4,20 +4,22 @@
 #include "analex.h"
 #include "anasint.h"
 #include <string.h>
-int main()
-{   
-    int i=0;
+int main(int argc, char *argv[])
+{
+    int i = 0;
     Token tx;
-    FILE *fp=fopen("teste.txt","r+");
-    if(fp==NULL){
+    FILE *fp = fopen(argv[1], "r+");
+    if (fp == NULL)
+    {
         printf("erro ao abrir arquivo");
         return 0;
     }
     char c;
-    while(!feof(fp)){
-        c=fgetc(fp);
-        tx=analex(c,fp);
-        i=anasint(&tx,i);
+    while (!feof(fp))
+    {
+        c = fgetc(fp);
+        tx = analex(c, fp);
+        i = anasint(&tx, i);
     }
     return 0;
 }
